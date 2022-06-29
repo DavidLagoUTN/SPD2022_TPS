e7d414b225d9e83fccda6e0de4ad42f317f3bba6
DavidLagoUTN <98719124+DavidLagoUTN@users.noreply.github.com>
2022-06-29 04:31:08 -0300

    Update calculadora_v0_41.ino

--- TP_02_Calculadora/calculadora_v0_41.ino
+++ TP_02_Calculadora/calculadora_v0_41.ino
@@ -49,10 +49,12 @@ int contadorSignoNegativo = 0;
 
 const byte ROWS = 4;
 const byte COLS = 4;
-char keys[ROWS][COLS] = { { '1', '2', '3', '+'},
-						  { '4', '5', '6', '-'},
-						  { '7','8', '9', '*' },
-						  { '_', '0', '=', '/'} };
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
 
 byte rowPins[ROWS] = { 12, 11, 10, 9 };
 byte colPins[COLS] = { 8, 7, 6, 5 };
